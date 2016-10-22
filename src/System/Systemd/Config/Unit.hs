315d61e28dd7440047229533d1fe8ea84b558faf
Tomasz Rybarczyk <paluho@gmail.com>
2016-10-22 23:25:24 +0200

    Fix tests

--- src/System/Systemd/Config/Unit.hs
+++ src/System/Systemd/Config/Unit.hs
@@ -17,9 +17,12 @@ instance Monoid Unit where
   mempty = Unit mempty
   mappend (Unit u1) (Unit u2) = Unit (u1 <> u2)
 
--- shorthand to handle optional value and derive
--- Monoid instances (which takes last defined value
--- from sum) for configuration data types
+-- | Shortcuts to handle optional values
+-- | (you can use `Value v` instead of `Last (Just v)`)
+-- | We are using `Last` instead of `Maybe`
+-- | to easily derive quite sane `Monoid` instances
+-- | (which take last defined value
+-- | from sum) for configuration data types
 pattern Value opt = Last (Just opt)
 pattern Missing = Last Nothing
 
@@ -31,9 +34,8 @@ section header values =
 
 printUnit :: Unit -> Text
 printUnit (Unit sections) =
-  intercalate "\n" (map buildSection sections)
+  intercalate "\n" (map buildSection . filter (not . null . snd) $ sections)
  where
-  buildSection (_, []) = ""
   buildSection (name, pairs) = "[" <> name <> "]\n" <> concat (map buildPair pairs)
   buildPair (name,value) = name <> "=" <> value <> "\n"
 
@@ -41,61 +43,3 @@ showBool :: Bool -> Text
 showBool True = "yes"
 showBool False = "no"
 
-data Architecture
-  = X86
-  | X86_64
-  | Ppc
-  | Ppc_le
-  | Ppc64
-  | Ppc64_le
-  | Ia64
-  | Parisc
-  | Parisc64
-  | S390
-  | S390x
-  | Sparc
-  | Sparc64
-  | Mips
-  | Mips_le
-  | Mips64
-  | Mips64_le
-  | Alpha
-  | Arm
-  | Arm_be
-  | Arm64
-  | Arm64_be
-  | Sh
-  | Sh64
-  | M86k
-  | Tilegx
-  | Cris
-  deriving (Generic, Show)
-
-showArchitecture :: Architecture -> Text
-showArchitecture X86 = "x86"
-showArchitecture X86_64 = "x86-64"
-showArchitecture Ppc = "ppc"
-showArchitecture Ppc_le = "ppc-le"
-showArchitecture Ppc64 = "ppc64"
-showArchitecture Ppc64_le = "ppc64-le"
-showArchitecture Ia64 = "ia64"
-showArchitecture Parisc = "parisc"
-showArchitecture Parisc64 = "parisc64"
-showArchitecture S390 = "s390"
-showArchitecture S390x = "s390x"
-showArchitecture Sparc = "sparc"
-showArchitecture Sparc64 = "sparc64"
-showArchitecture Mips = "mips"
-showArchitecture Mips_le = "mips-le"
-showArchitecture Mips64 = "mips64"
-showArchitecture Mips64_le = "mips64-le"
-showArchitecture Alpha = "alpha"
-showArchitecture Arm = "arm"
-showArchitecture Arm_be = "arm-be"
-showArchitecture Arm64 = "arm64"
-showArchitecture Arm64_be = "arm64-be"
-showArchitecture Sh = "sh"
-showArchitecture Sh64 = "sh64"
-showArchitecture M86k = "m86k"
-showArchitecture Tilegx = "tilegx"
-showArchitecture Cris = "cris"
--- test/System/Systemd/Config/Networkd/NetDevSpec.hs
+++ test/System/Systemd/Config/Networkd/NetDevSpec.hs
@@ -4,22 +4,22 @@ import           Data.Text (unlines)
 import           Net.Mac (fromOctets)
 import           Prelude hiding (unlines)
 import           Test.Hspec (describe, it, shouldBe, Spec)
-import           System.Systemd.Config.Networkd.NetDev (emptyBridge, toUnit, netDev, Kind(BridgeKind), NetDev(..))
-import           System.Systemd.Config.Unit (printUnit)
+import           System.Systemd.Config.Networkd.NetDev (toUnit, netDev, Device(BridgeDevice), NetDev(..))
+import           System.Systemd.Config.Unit (printUnit, pattern Value)
 
 suite :: Spec
 suite =
   describe "System.Systemd.Config.Networkd.NetDev" $ do
     it "generates correct unit for empty config" $
-      (printUnit . toUnit $ netDev "br0" (BridgeKind emptyBridge)) `shouldBe` "[NetDev]\nKind=bridge\nName=br0\n"
+      (printUnit . toUnit $ netDev "br0" (BridgeDevice mempty)) `shouldBe` "[NetDev]\nKind=bridge\nName=br0\n"
     it "generates correct unit for fully customized config" $
       shouldBe
         (printUnit . toUnit $
           NetDev
-            { netDevDescription = Just "A free-form description..."
-            , netDevKind = BridgeKind emptyBridge
-            , netDevMtuBytes = Just 1500
-            , netDevMacAddress = Just (fromOctets 0xf0 0xde 0xf1 0x62 0x90 0x55)
+            { netDevDescription = Value "A free-form description..."
+            , netDevKind = BridgeDevice mempty
+            , netDevMtuBytes = Value 1500
+            , netDevMacAddress = Value (fromOctets 0xf0 0xde 0xf1 0x62 0x90 0x55)
             , netDevName = "br0"
             })
         (unlines
